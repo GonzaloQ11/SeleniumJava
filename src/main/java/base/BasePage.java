82aba8078a16a071b4c5e0c6fa2e1070d0882c8b
Gonzalo Quispe <gonzalo.quispe.dev@gmail.com>
2025-03-24 12:00:09 -0300

    add pageFinishedLoading in BasePage

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,16 +1,20 @@
 package base;
 
 import config.ConfigReader;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.time.Duration;
 
 public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected final By requiredInputError = By.xpath("//parent::*//following-sibling::span");
     private static final int DEFAULT_WAIT_DURATION = Integer.parseInt(ConfigReader.getProperty("defaultWait"));
 
     public BasePage(WebDriver driver) {
@@ -24,5 +28,9 @@ public class BasePage {
 
     protected boolean isMessageDisplayed(WebElement element, String text) {
        return wait.until(ExpectedConditions.textToBePresentInElement(element, text));
+    }
+
+    public void pageFinishedLoading() {
+        wait.until(driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete"));
     }
 }
